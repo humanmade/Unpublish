bb4a4078233472ba53431b837470d4e1972f57ac
Dzikri Aziz <kvcrvt@gmail.com>
2016-07-25 14:29:28 +0800

    Clear timestamp when clicking on Clear

--- js/unpublish.js
+++ js/unpublish.js
@@ -1,6 +1,7 @@
 (function( $ ) {
 	var $editLink = $( '.edit-unpublish-timestamp' );
 	var $fieldset = $( '#unpublish-timestampdiv' );
+	var $stamp    = $( '#unpublish-timestamp strong' );
 
 	var hideFieldset = function() {
 		$fieldset.slideUp( 'fast' );
@@ -32,8 +33,7 @@
 	$( '.clear-unpublish-timestamp' ).on( 'click', function( e ) {
 		e.preventDefault();
 		$fieldset.find( ':input' ).val( '' );
-
-		// TODO: Update stamp.
+		$stamp.html( '&mdash;' );
 	});
 
 	$( '.save-unpublish-timestamp' ).on( 'click', function( e ) {
