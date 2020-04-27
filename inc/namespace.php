b947c0796c33c07e3cfca680edb6ef8f49940a76
Dzikri Aziz <dzikri@humanmade.com>
2020-04-27 22:38:49 +0800

    Import unschedule_unpublish() to main namespace file

--- inc/namespace.php
+++ inc/namespace.php
@@ -14,3 +14,16 @@ const POST_META_KEY = 'unpublish_timestamp';
  * @return void
  */
 function bootstrap(): void {}
+
+/**
+ * Unschedule unpublishing post
+ *
+ * @param int $post_id Post ID.
+ *
+ * @return mixed On success an integer indicating number of events unscheduled
+ *               (0 indicates no events were registered with the hook and
+ *               arguments combination), false if unscheduling one or more events fail.
+ */
+function unschedule_unpublish( int $post_id ) {
+	return wp_clear_scheduled_hook( self::$cron_key, array( $post_id ) );
+}
