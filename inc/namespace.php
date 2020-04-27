7315448c3c624f670d796b71fbd4b3403c6bede0
Dzikri Aziz <dzikri@humanmade.com>
2020-04-27 22:17:13 +0800

    Add initial namespace file

---
+++ inc/namespace.php
@@ -0,0 +1,16 @@
+<?php
+
+declare( strict_types = 1 );
+
+namespace HM\Unpublish;
+
+const CRON_KEY = 'unpublish_post_cron';
+const FEATURE_NAME = 'unpublish';
+const POST_META_KEY = 'unpublish_timestamp';
+
+/**
+ * Bootstrapper
+ *
+ * @return void
+ */
+function bootstrap(): void {}
