b5ae66ef413e4361b86c330f5e851619bb74abeb
Dzikri Aziz <dzikri@humanmade.com>
2020-04-29 02:53:47 +0800

    Add component: Panel

---
+++ assets/src/components/Panel/index.js
@@ -0,0 +1,6 @@
+import React from '@wordpress/element';
+import { PluginPostStatusInfo } from '@wordpress/edit-post';
+
+export default function Panel() {
+	return <PluginPostStatusInfo className="unpublish">TODO</PluginPostStatusInfo>;
+}
