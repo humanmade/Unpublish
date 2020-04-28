16bde232aaba1ffb0e7de980fb7896ff663cd0bc
Dzikri Aziz <dzikri@humanmade.com>
2020-04-29 03:04:30 +0800

    Add panel label

--- assets/src/components/Panel/index.js
+++ assets/src/components/Panel/index.js
@@ -1,6 +1,11 @@
 import React from '@wordpress/element';
+import { __ } from '@wordpress/i18n';
 import { PluginPostStatusInfo } from '@wordpress/edit-post';
 
 export default function Panel() {
-	return <PluginPostStatusInfo className="unpublish">TODO</PluginPostStatusInfo>;
+	return (
+		<PluginPostStatusInfo className="unpublish">
+			<span>{ __( 'Unpublish', 'unpublish' ) }</span>
+		</PluginPostStatusInfo>
+	);
 }
