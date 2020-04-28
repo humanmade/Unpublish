c385382eb2eefa7ab699b5e3bd0414e23757a90e
Dzikri Aziz <dzikri@humanmade.com>
2020-04-29 03:11:40 +0800

    Render calendar

--- assets/src/components/Panel/index.js
+++ assets/src/components/Panel/index.js
@@ -1,11 +1,30 @@
 import React from '@wordpress/element';
 import { __ } from '@wordpress/i18n';
 import { PluginPostStatusInfo } from '@wordpress/edit-post';
+import { Dropdown, Button } from '@wordpress/components';
+import { PostSchedule as PostScheduleForm } from '@wordpress/editor';
 
 export default function Panel() {
 	return (
 		<PluginPostStatusInfo className="unpublish">
 			<span>{ __( 'Unpublish', 'unpublish' ) }</span>
+			<Dropdown
+				position="bottom left"
+				contentClassName="edit-post-post-unpublish__dialog"
+				renderToggle={ ( { onToggle, isOpen } ) => (
+					<>
+						<Button
+							className="edit-post-post-unpublish__dialog"
+							onClick={ onToggle }
+							aria-expanded={ isOpen }
+							isLink
+						>
+							{ __( 'Schedule' ) }
+						</Button>
+					</>
+				) }
+				renderContent={ () => <PostScheduleForm /> }
+			/>
 		</PluginPostStatusInfo>
 	);
 }
