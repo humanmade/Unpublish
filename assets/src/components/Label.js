002279687576132aa102ebddb8cd1b147839b2d0
Dzikri Aziz <dzikri@humanmade.com>
2020-04-29 23:14:44 +0800

    Label: Format date

--- assets/src/components/Label.js
+++ assets/src/components/Label.js
@@ -2,23 +2,32 @@ import React from '@wordpress/element';
 import PropTypes from 'prop-types';
 import { __ } from '@wordpress/i18n';
 import { Button } from '@wordpress/components';
+import { __experimentalGetSettings, dateI18n } from '@wordpress/date';
 
-function getLabel() {
-	return __( 'Schedule' );
+function getLabel( date ) {
+	if ( ! date ) {
+		return __( 'Schedule' );
+	}
+
+	const { formats } = __experimentalGetSettings();
+	const { date: dateFormat, time: timeFormat } = formats;
+
+	return dateI18n( `${ dateFormat } ${ timeFormat }`, date );
 }
 
 export default function Label( props ) {
-	const { className, isOpen, onToggle } = props;
+	const { className, date, isOpen, onToggle } = props;
 
 	return (
 		<Button isLink className={ className } onClick={ onToggle } aria-expanded={ isOpen }>
-			{ getLabel() }
+			{ getLabel( date ) }
 		</Button>
 	);
 }
 
 Label.propTypes = {
 	className: PropTypes.string.isRequired,
+	date: PropTypes.number.isRequired,
 	isOpen: PropTypes.bool.isRequired,
 	onToggle: PropTypes.func.isRequired,
 };
