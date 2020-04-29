b1785d385c77752ced4980629fd8c4df9eb4631d
Dzikri Aziz <dzikri@humanmade.com>
2020-04-29 19:59:51 +0800

    Split Toggle

--- assets/src/components/Panel/index.js
+++ assets/src/components/Panel/index.js
@@ -4,7 +4,15 @@ import { PluginPostStatusInfo } from '@wordpress/edit-post';
 import { Dropdown, Button } from '@wordpress/components';
 
 import Field from '../Field';
-import UnpublishLabel from '../UnpublishLabel';
+import Label from '../Label';
+
+function Toggle( { onToggle, isOpen } ) {
+	return (
+		<Button className="edit-post-post-unpublish__dialog" onClick={ onToggle } aria-expanded={ isOpen } isLink>
+			<Label />
+		</Button>
+	);
+}
 
 export default function Panel() {
 	return (
@@ -13,18 +21,7 @@ export default function Panel() {
 			<Dropdown
 				position="bottom left"
 				contentClassName="edit-post-post-unpublish__dialog"
-				renderToggle={ ( { onToggle, isOpen } ) => (
-					<>
-						<Button
-							className="edit-post-post-unpublish__dialog"
-							onClick={ onToggle }
-							aria-expanded={ isOpen }
-							isLink
-						>
-							<UnpublishLabel />
-						</Button>
-					</>
-				) }
+				renderToggle={ props => <Toggle { ...props } /> }
 				renderContent={ () => <Field /> }
 			/>
 		</PluginPostStatusInfo>
